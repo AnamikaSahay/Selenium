583d99aff27a660b6e68fcbc5fecf851286b43be
Anamika Sahay <sahay.anamika@gmail.com>
2017-02-17 13:37:38 -0800

    try

---
+++ src/test/java/com/test/Sample/AppTest.java
@@ -0,0 +1,64 @@
+package com.test.Sample;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void testLogin(){
+		System.out.println("Testing Login test case");
+	}
+	
+	@Test
+	public void testUserregisteration(){
+		Assert.fail();
+		System.out.println("Testing User registeration");
+		Assert.fail();
+	}
+	
+	@BeforeMethod
+	public void Openbrowser(){
+		
+		System.out.println("Open broswser");		
+	}
+	
+	@BeforeTest
+	public void openDb(){
+		
+		System.out.println("Open db connection");
+	}
+	
+	@AfterMethod
+	public void Closebrowser(){
+		
+		System.out.println("Close browsers");
+		
+	}
+	
+	@AfterTest
+	public void closeDB(){
+		
+		System.out.println("Close db connection");
+	}
+	
+	
+	@BeforeSuite
+	public void runSeleniumserver(){
+		
+		System.out.println("Testing running of selenium server");
+	}
+	
+	@AfterSuite
+	public void stopSeleniumserver(){
+		
+		System.out.println("Close selenium server");
+	}
+	
+}
---
+++ src/test/java/com/test/Sample/AppTest2.java
@@ -0,0 +1,12 @@
+package com.test.Sample;
+
+import org.testng.annotations.Test;
+
+public class AppTest2 {
+
+	@Test
+	public void testCapturescreeshot(){
+		System.out.println("Second test case - screenshot capture");
+	//throw new SkipException("Skipping the test case");	
+	}
+}
